86f5dc8f5658d21ff440851692573c2164154f60
duphatminh <duphatminhh@gmail.com>
2022-11-18 14:09:09 +0700

    Init Config Babel

--- src/utilities/sorts.js
+++ src/utilities/sorts.js
@@ -1,7 +1,5 @@
 // Sắp xếp một array dựa trên 1 array khác
-const mapOrder = (array, order, key) => {
+export const mapOrder = (array, order, key) => {
     array.sort((a,b) => order.indexOf(a[key]) - order.indexOf(b[key]))
     return array
 }
-
-export { mapOrder }
