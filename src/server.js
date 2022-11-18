2fc3cfde2ebc949fbbbea0560895368f70bd92ca
duphatminh <duphatminhh@gmail.com>
2022-11-18 16:14:17 +0700

    Init Config MongoDB

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 import express from 'express'
+import { connectDB } from './config/mongodb.js'
 import { mapOrder } from './utilities/sorts.js'
+import { env } from './config/environment'
 
 const app = express()
 
-const hostname = 'localhost'
-const port = 7722
+connectDB().catch(console.log)
 
 app.get('/',(reg, res) => {
     res.end('<h1>Hello World</h1><hr/>')
 }) 
 
-app.listen(port,hostname, () => {
-    console.log('Hello ${hostname}:${port}')
+app.listen(env.PORT,env.HOST, () => {
+    console.log('Hello ',env.HOST+':'+env.PORT)
 })
