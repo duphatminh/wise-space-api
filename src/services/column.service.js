71ae8b26d2d5214cf5cba0750df32a06699c5fa7
duphatminh <duphatminhh@gmail.com>
2022-11-23 01:45:02 +0700

    Deploy Wise Space

--- src/services/column.service.js
+++ src/services/column.service.js
@@ -1,6 +1,6 @@
-import { ColumnModel } from '*/models/Column.model'
-import { BoardModel } from '*/models/Board.model'
-import { CardModel } from '*/models/Card.model'
+import { ColumnModel } from '../models/Column.model'
+import { BoardModel } from '../models/Board.model'
+import { CardModel } from '../models/Card.model'
 
 const createNew = async (data) => {
     try {
